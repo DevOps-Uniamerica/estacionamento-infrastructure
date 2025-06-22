43c47b6f56d2fee124381b6a0bf3b92ea44b002a
Andre Mai Velasco <andremaivelasco@hotmail.com>
2025-06-22 01:22:52 -0300

    ajusteo infra k8

--- terraform/application/infra.tf
+++ terraform/application/infra.tf
@@ -8,7 +8,6 @@ resource "google_container_cluster" "k8s" {
   location = "southamerica-east1"
 
   remove_default_node_pool = true
-  initial_node_count       = 1
 
   network    = "default"
   subnetwork = "default"
