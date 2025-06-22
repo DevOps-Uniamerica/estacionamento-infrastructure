203478c15fbe2e5d2d188ac764d91955907fe384
Andre Mai Velasco <andremaivelasco@hotmail.com>
2025-06-22 01:11:42 -0300

    atualização infra k8

--- terraform/application/infra.tf
+++ terraform/application/infra.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.38.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project
   region  = "southamerica-east1"
